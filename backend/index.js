2b5bfad32079d6b728d3934407d0b7d9961841d3
Jaidev Das <jaidevkr2@gmail.com>
2020-07-02 12:52:06 +0530

    Changed port of backend

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,23 @@
-const express=require('express'),
-		http=require('http');
+const express = require("express"),
+  http = require("http");
 
-const hostname='localhost';
-const port =3000;
+const hostname = "localhost";
+const port = 5000;
 
-const morgan=require('morgan');
+const morgan = require("morgan");
 
+const app = express();
+const bodyParser = require("body-parser");
 
-const app=express();
-const bodyParser=require('body-parser');
-
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-const subdomainrouter=require('./routes/subdomain');
+const subdomainrouter = require("./routes/subdomain");
 
-app.use('/subdomain',subdomainrouter);
+app.use("/subdomain", subdomainrouter);
 
-const server=http.createServer(app);
+const server = http.createServer(app);
 
-server.listen(port,hostname,()=>{
-	console.log(`Server running at http://${hostname}:${port}`);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
 });
